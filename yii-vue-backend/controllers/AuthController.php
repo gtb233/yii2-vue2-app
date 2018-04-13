0a569b765d9bab1d1dd3641c17513bc0eda1e26b
gaotanbin <tanbin.gao@g-emall.com>
2018-04-13 18:33:38 +0800

    init

---
+++ yii-vue-backend/controllers/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 身份认证
+ * Date: 2018/4/13
+ * Time: 15:50
+ */
+
+namespace app\controllers;
+
+
+class AuthController extends BaseApiController
+{
+    public function actionIndex()
+    {
+        $username = \yii::$app->request->post('name');
+        $password = \yii::$app->request->post('password');
+        if ($username == 'admin' && $password == 'admin'){
+            return ['success' => 1, 'msg' => '100-token'];
+        }
+        return ['success' => 0, 'msg' => \yii::t('erp', 'username or password error!')];
+    }
+}
